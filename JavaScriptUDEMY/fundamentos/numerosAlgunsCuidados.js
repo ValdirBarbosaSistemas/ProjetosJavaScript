57c58f190063da4bff43f6ca738e724bca2396b8
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2020-08-20 22:52:14 -0300

    Mudança em 'numeroAlgunsCuidados'

--- JavaScriptUDEMY/fundamentos/numerosAlgunsCuidados.js
+++ JavaScriptUDEMY/fundamentos/numerosAlgunsCuidados.js
@@ -1,4 +1,4 @@
 console.log(7 / 0) // qualquer linguagem de programação daria um erro. No JS não, mostra 'infinity'
 console.log("10" / 2) // em algumas linguagens daria erro. No JS o resultado é '5'
 console.log("Show!" * 2) // em algumas linguagens de programação multiplicaria o 'Show' 2 vezes. No JS não. dá 'NaN'
-console.log(0.1 + 0.7) // Em algumas linguagens de programação daria '0.8'. No JS não. Dá 'NaN'
+console.log(0.1 + 0.7) // Em algumas linguagens de programação daria '0.8'. No JS não. Dá 'NaN' (Not a Number)
