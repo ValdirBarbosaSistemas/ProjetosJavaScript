932d11e7085892e52cd0306d6fe9b0ff3db3def1
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2020-08-20 22:41:13 -0300

    Mudanças no arquivo Number js

--- JavaScriptUDEMY/fundamentos/numeros.js
+++ JavaScriptUDEMY/fundamentos/numeros.js
@@ -3,7 +3,7 @@ const peso2 = Number('2.0') //outra maneira de declarar um number
 
 console.log(peso1)
 console.log(peso2)
-console.log(Number.isInteger(peso1)) //Dizer que tipo de Number é (inteiro ou não)
+console.log(Number.isInteger(peso1)) //Dizer que tipo de Number é (inteiro ou não) retorna boolean
 console.log(Number.isInteger(peso2))
 
 const avaliacao01 = 9.871
@@ -13,4 +13,4 @@ const total = avaliacao01 * peso1 + avaliacao02 * peso2
 const media = total / (peso1 + peso2)
 
 console.log(media1.tofixed(2)) //O método 'tofixed' serve para colocar a quantidade de casas decimais na operação
-console.log(media.toString) // 'to string' serve para converter o number em String
+console.log(media.toString) // 'to string' serve para converter o number em String1
