de8030320486b74061dc378e2a6fe3dc54011daa
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2020-08-20 23:01:29 -0300

    Mudanças no 'math'

--- JavaScriptUDEMY/fundamentos/math.js
+++ JavaScriptUDEMY/fundamentos/math.js
@@ -1,5 +1,5 @@
 const raio = 5.6
-const area = Math.PI * Math.pow(raio, 2)
+const area = Math.PI * Math.pow(raio, 2) // Passando 2 parâmetros que no caso vai ser 'raio ao quadrado'
 
 console.log(area)
 console.log(typeof Math)
