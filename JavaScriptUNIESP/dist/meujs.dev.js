a7d5e05ea327690da9a322bc62db964545df9b92
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2020-09-16 15:50:22 -0300

    Modificações nos arquivos aula01_js e meujs

--- JavaScriptUNIESP/dist/meujs.dev.js
+++ JavaScriptUNIESP/dist/meujs.dev.js
@@ -2,3 +2,11 @@
 
 document.getElementById("ligar").innerHTML = "ligar";
 document.getElementById("desligar").innerHTML = "desligar";
+
+function ligar() {
+  document.getElementById("lampada").src = "pic_bulbon.gif";
+}
+
+function desligar() {
+  document.getElementById("lampada").src = "pic_bulboff.gif";
+}
--- JavaScriptUNIESP/meujs.js
+++ JavaScriptUNIESP/meujs.js
@@ -1,2 +1,10 @@
 document.getElementById("ligar").innerHTML = "ligar";
 document.getElementById("desligar").innerHTML = "desligar";
+
+function ligar() {
+  document.getElementById("lampada").src = "pic_bulbon.gif";
+}
+
+function desligar() {
+  document.getElementById("lampada").src = "pic_bulboff.gif";
+}
