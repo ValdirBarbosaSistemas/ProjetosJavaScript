adbdb6b850b850b25618e3f5eebb023cdd0ebb7f
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2021-11-10 22:10:43 -0300

    Modificação de alguns arquivos

--- JavaScriptUDEMY/função/Factory02.js
+++ JavaScriptUDEMY/função/Factory02.js
@@ -1,10 +1,12 @@
+//Outra forma de se criar objetos é por parâmetros
+
 function criarProduto(nome, preco) {
   return {
-    nome,
+    nome, //Não precisa colocar "nome: nome" pois ja foi passado pelo parâmetro
     preco,
-    desconto: 0.1,
+    desconto: 0.1, //Valor padrão
   };
 }
 
-console.log(criarProduto("Notebook", 2199));
+console.log(criarProduto("Notebook", 2199)); //Criação dos objetos através dos parâmetros
 console.log(criarProduto("Ipad", 1199.49));
