50b4e969f9e8351affdb0172075d6b94d738030c
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2021-11-22 13:41:41 -0300

    Adicionando novos arquivos JS

---
+++ JavaScriptUDEMY/objeto/objetosConstantes.js
@@ -0,0 +1,20 @@
+//pessoa -> 123 -> {...}
+const pessoa = { nome: "João" };
+pessoa.nome = "Pedro";
+console.log(pessoa); //Aqui o resultado será {Pedro}
+
+/**Em objetos, pode-se mudar um conteúdo de sua variável mesmo estando constante.
+ * Para se ter um "objeto constante" tem que se usar o método "Freeze";
+ */
+Object.freeze(pessoa);
+
+pessoa.nome = "Maria"; //Quando se usa o "Freeze" não se pode ALTERAR
+pessoa.end = "Rua abc"; //Não se pode INCLUIR
+delete pessoa.nome; //Não se pode DELETAR
+
+console.log(pessoa.nome);
+console.log(pessoa);
+
+//Outra forma de criar objetos constantes é durante sua criação...
+const pessoaConstante = Object.freeze({ nome: "João" });
+console.log(pessoaConstante);
